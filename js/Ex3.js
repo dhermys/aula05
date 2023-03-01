1f192dea6ea026b4acfdd38f9dcc82c7f0220f72
Diego <diegohermys@gmail.com>
2023-03-01 10:57:40 -0300

    Refatoração ex3

--- js/Ex3.js
+++ js/Ex3.js
@@ -1,22 +1,17 @@
 document.getElementById("btn").onclick = iniciarTroca;
 document.getElementById("btn2").onclick = pararTroca;
 
-var counter = 1
-function iniciarTroca() {
+function troca() {
     const elem = document.getElementById("trocar")
     var color = ['blue', 'brown', 'red', 'green', 'yellow', 'orange', 'pink', 'purple']
+    elem.style.color = color[parseInt(Math.random() * 9)]
+    console.log(elem.style.color)
+}
 
-    var timer = setInterval(function () {
-        if (counter < 1) {
-            clearInterval(timer)
-        }
-        elem.style.color = color[parseInt(Math.random() * 9)]
-        console.log(elem.style.color)
-    }, 1000);
+function iniciarTroca() {
+    time = setInterval(troca, 1000);
 }
 
 function pararTroca() {
-    setTimeout(() => {return counter = 1}, 500);
-    return counter = 0
+    clearInterval(time);
 }
-
